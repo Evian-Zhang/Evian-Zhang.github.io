fb2ae46e2784fa061a9119b7a66c3240981301c8
Evian-Zhang <evianzhang1999@163.com>
2019-02-16 18:54:52 +0800

    add the color and position by css

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+function setCanvasSize() {
+    var connector = document.getElementById("connector");
+    var canvasContainer = document.getElementById("canvasContainer");
+    connector.style.width = canvasContainer.scrollWidth + "px";
+    connector.style.height = canvasContainer.scrollHeight + "px";
+}
 window.onload = function () {
+    setCanvasSize();
+    var connector = document.getElementById("connector");
+    if (connector.getContext) {
+        var context = connector.getContext("2d");
+        context.clearRect(0, 0, connector.width, connector.height);
+        context.fillStyle = "red"; // 指定填充颜色为红色
+        context.fillRect(10, 10, 150, 150);
+    }
+};
+window.onresize = function () {
+    setCanvasSize();
+    var connector = document.getElementById("connector");
+    if (connector.getContext) {
+        var context = connector.getContext("2d");
+        context.clearRect(0, 0, connector.width, connector.height);
+        context.fillStyle = "red"; // 指定填充颜色为红色
+        context.fillRect(10, 10, 150, 150);
+    }
 };
--- index.ts
+++ index.ts
@@ -1,2 +1,28 @@
+function setCanvasSize () {
+    let connector = document.getElementById("connector") as HTMLCanvasElement;
+    let canvasContainer = document.getElementById("canvasContainer") as HTMLDivElement;
+    connector.style.width = canvasContainer.scrollWidth + "px";
+    connector.style.height = canvasContainer.scrollHeight + "px";
+}
+
 window.onload = function () {
+    setCanvasSize();
+    let connector = document.getElementById("connector") as HTMLCanvasElement;
+    if (connector.getContext) {
+        var context = connector.getContext("2d");
+        context.clearRect(0, 0, connector.width, connector.height);
+        context.fillStyle = "red"; // 指定填充颜色为红色
+        context.fillRect(10, 10, 150, 150);
+    }
+};
+
+window.onresize = function () {
+    setCanvasSize();
+    let connector = document.getElementById("connector") as HTMLCanvasElement;
+    if (connector.getContext) {
+        var context = connector.getContext("2d");
+        context.clearRect(0, 0, connector.width, connector.height);
+        context.fillStyle = "red"; // 指定填充颜色为红色
+        context.fillRect(10, 10, 150, 150);
+    }
 };

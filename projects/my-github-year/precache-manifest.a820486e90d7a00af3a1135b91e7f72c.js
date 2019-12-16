d0d2089aa3f07c5f875c6d014a0150ad6f82cb2f
Evian-Zhang <evianzhang1999@163.com>
2019-12-17 00:20:50 +0800

    add my-github-year

---
+++ projects/my-github-year/precache-manifest.a820486e90d7a00af3a1135b91e7f72c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbc4050e234382e956abd66233294a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e71a0e84cf776a6a2592",
+    "url": "./static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "3360f50fd0967f43f596",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e71a0e84cf776a6a2592",
+    "url": "./static/js/2.cef78030.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.cef78030.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3360f50fd0967f43f596",
+    "url": "./static/js/main.4c1ea4b2.chunk.js"
+  },
+  {
+    "revision": "efd882131ddf041639ca",
+    "url": "./static/js/runtime-main.cd482c53.js"
+  }
+]);
